cdd13624fee2c406aab44b0cf75d3db4e517ced3
KieHr <50412162+keiHR@users.noreply.github.com>
2021-05-05 17:48:32 +0900

    Update SG90servo_ver2.py

--- test/src/mock-up/SG90servo_ver2.py
+++ test/src/mock-up/SG90servo_ver2.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import main
 
 class SG90servo():
     def __init__(self,motorport=2):
@@ -11,6 +12,7 @@ class SG90servo():
         self.minlim = -90
         self.maxlim = 90
 
+    # サーボモータを動かす関数
     def servo_deg(self,deg):
 
         if deg > self.maxlim:
@@ -23,7 +25,31 @@ class SG90servo():
         cycle = 7.25+4.75*deg/90.0
         self.servo.ChangeDutyCycle(cycle)
         
-    def tracking(self, deg):
+        
+    #トラッキング用の関数
+    def tracking(self, type, deg, center, bin, target, step):
+        
+        # 最大回転角度
+        max = 90
+        # 最小回転角度 typeが0：水平回転　typeが1：垂直回転
+        if type == 0:
+            min = -90
+        else:
+            min = -30
+        
+        # 回転するか否かを判定する閾値　カメラの中心と対象物の距離が閾値よりも大きいとトラッキングするようにする
+        threshold = center / bin
+        
+        # モータの回転制御
+        if center < target and abs(center - target) > threshold:
+            deg -= step
+            if deg < min:
+                deg = min
+        elif center > target and abs(center - target) > threshold:
+            deg += step
+            if deg > max:
+                deg = max
+        self.servo_deg(deg)
         
 
     def stop(self):
